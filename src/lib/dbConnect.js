09eef8fc962abed6f20bf0893ad7a4de3cfb94ff
eagle9111 <alhassan.khalilnew@gmail.com>
2025-08-01 21:22:11 +0300

    db updates

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -7,8 +7,14 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+   ssl: {
+    ca: fs.readFileSync(process.env.CA)
+   }
+ });
 
 db.connect(err => {
   if (err) {
